be87c1ab7ddc78a6c5199d93e5a9488beb8b51a4
James Ouyang <52713371+jamesouyang2003@users.noreply.github.com>
2022-06-28 18:08:33 -0700

    Add facing to PlayerController

--- EGaDS! Open Project Game/Assets/Scripts/AbilitySystem/PlayerController.cs
+++ EGaDS! Open Project Game/Assets/Scripts/AbilitySystem/PlayerController.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum Facings { LEFT = -1, RIGHT = 1 };
+
     [SerializeField] private float _moveSpeed = 10;
     [SerializeField] private float _fallSpeed = 20;
-    [SerializeField] private float _wallSlide = 10;
+    [SerializeField] private float _wallSlide = 8;
     [SerializeField] private float _jumpHeight = 4;
 
     // time after wall jumping until left right movement is given back to player
-    [SerializeField] private float _wallJumpTime = 0.5f;
+    [SerializeField] private float _wallJumpTime = 0.3f;
 
     [Header("Ground and Wall Detection")]
     [SerializeField] private LayerMask _groundLayerMask;
     [SerializeField] private float _groundRaycastLength = .5f;
-    [SerializeField] private float _groundDetectionWidth = 1f;
-    [SerializeField] private float _wallRaycastLength = 0.3f;
-    [SerializeField] private float _wallDetectionWidth = 1f;
-    [SerializeField] private float _wallDetectionHeight = 0.7f;
+    [SerializeField] private float _groundDetectionWidth = .8f;
+    [SerializeField] private float _wallRaycastLength = .3f;
+    [SerializeField] private float _wallDetectionWidth = .8f;
+    [SerializeField] private float _wallDetectionHeight = .7f;
 
+
+    /// <summary>
+    /// The maximum left-right movement speed of the player, in tiles per second
+    /// </summary>
     public float MoveSpeed => _moveSpeed * (_abilityManager?.SpeedMultiplier ?? 1);
+
+    /// <summary>
+    /// The maximum fall speed of the player, in tiles per second
+    /// </summary>
     public float FallSpeed => _fallSpeed * (_abilityManager?.FallSpeedMultiplier ?? 1);
+
+    /// <summary>
+    /// The maximum wall slide speed of the player, in tiles per second
+    /// </summary>
     public float WallSlideSpeed => _wallSlide * (_abilityManager?.WallSlideMultiplier ?? 1);
-    public float JumpHeight => _jumpHeight + (_abilityManager?.JumpHeightAddend ?? 0);
+
+    /// <summary>
+    /// The jump height of the player in number of tiles
+    /// </summary>
+    public float JumpHeight => _jumpHeight + .5f + (_abilityManager?.JumpHeightAddend ?? 0);
+
+    /// <summary>
+    /// The number of air jumps the player can do
+    /// </summary>
     public int AirJumpCount => _abilityManager?.AirJumpAddend ?? 0;
 
+
+    /// <summary>
+    /// The direction the player is facing (either left or right)
+    /// </summary>
+    public Facings Facing { get; private set; } = Facings.RIGHT;
+
+
+    /// <summary>
+    /// True when the player is on the ground
+    /// </summary>
     public bool IsGrounded
     {
         get
@@ -38,6 +70,8 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // Performs a raycast on the specified side of the player
+    // Use Vector.left or Vector.right as input
     private bool IsSideOnWall(Vector2 side)
     {
         Vector3 feetPosition = transform.position - new Vector3(0, transform.localScale.y/2, 0);
@@ -47,15 +81,29 @@ public class PlayerController : MonoBehaviour
             || Physics2D.Raycast(feetPosition+edgeTransform+heightTransform, side, _wallRaycastLength, _groundLayerMask);
     }
 
+    /// <summary>
+    /// True when the player is on a wall to their left side
+    /// </summary>
     public bool IsLeftOnWall => IsSideOnWall(Vector2.left);
+
+    /// <summary>
+    /// True when the player is on a wall to their right side
+    /// </summary>
     public bool IsRightOnWall => IsSideOnWall(Vector2.right);
+
+    /// <summary>
+    /// True when the player is on a wall
+    /// </summary>
     public bool IsOnWall => IsLeftOnWall || IsRightOnWall;
+
 
     private Rigidbody2D _rigidbody;
     private BoxCollider2D _collider;
     private AbilityManager _abilityManager;
 
     private float _wallJumpTimer = -1;
+    private float _currentAirJumpCount = 0;
+
 
     private void Start()
     {
@@ -72,20 +120,38 @@ public class PlayerController : MonoBehaviour
         {
             var moveDirection = Input.GetAxisRaw("Horizontal");
             _rigidbody.velocity = new Vector2(moveDirection*MoveSpeed, _rigidbody.velocity.y);
+
+            if (moveDirection != 0) // update facing
+                Facing = moveDirection == 1 ? Facings.RIGHT : Facings.LEFT;
         }
 
         if (Input.GetButtonDown("Jump"))
         {
             var jumpSpeed = Mathf.Sqrt(-2*Physics2D.gravity.y*_rigidbody.gravityScale*JumpHeight);
-            if (IsGrounded)
+
+            if (IsGrounded) // perform ground jump
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpSpeed);
-            else if (IsOnWall)
+            
+            else if (IsOnWall) // perform wall jump
             {
-                var wallJumpVelocity = IsLeftOnWall ? MoveSpeed : -MoveSpeed;
+                Facing = IsLeftOnWall ? Facings.RIGHT : Facings.LEFT;
+                var wallJumpVelocity = (int)Facing * MoveSpeed;
                 _rigidbody.velocity = new Vector2(wallJumpVelocity, jumpSpeed);
                 _wallJumpTimer = _wallJumpTime;
             }
+            
+            else if (_currentAirJumpCount < AirJumpCount) // perform air jump
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpSpeed);
+                _currentAirJumpCount++;
+            }
         }
+
+        // reset air jump count
+        if (IsGrounded || IsOnWall) _currentAirJumpCount = 0;
+
+        // Update direction player is facing based on Facing value
+        transform.localScale = new Vector3((int)Facing, 1, 1);
     }
 
     void FixedUpdate()
