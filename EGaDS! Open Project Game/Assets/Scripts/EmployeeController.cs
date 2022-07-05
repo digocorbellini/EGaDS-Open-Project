fc3909f9c1c6641627960d864857d6c33c887adb
jamesdrayton <56940014+jamesdrayton@users.noreply.github.com>
2022-07-04 21:04:16 -0700

    Add files via upload

--- EGaDS! Open Project Game/Assets/Scripts/EmployeeController.cs
+++ EGaDS! Open Project Game/Assets/Scripts/EmployeeController.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class EmployeeController : MonoBehaviour {
 
-    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private Transform[] _waypoints;
     // controls the movespeed of the employee
-    [SerializeField] private float moveSpeed = 10;
+    [SerializeField] private float _moveSpeed = 10;
     // true if the employee loops waypoints, false if it stops at the last waypoint 
-    [SerializeField] private bool looping = true;
+    [SerializeField] private bool _looping = true;
     // controls the amount of damage dealt to the player upon touching the employee
-    [SerializeField] private int damage = 1;
+    [SerializeField] private int _damage = 1;
     private int nextWaypoint = 1;
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start() {
         // Sets the tired employee enemy to begin at the first waypoint
-        transform.position = waypoints[0].transform.position;
+        transform.position = _waypoints[0].transform.position;
     }
 
     // Update is called once per frame
     void Update() {
         checkWaypoint();
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[nextWaypoint].position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, _waypoints[nextWaypoint].position, _moveSpeed * Time.deltaTime);
     }
 
     // Checks if Employee has reached nextWaypoint and updates nextWaypoint
     void checkWaypoint() {
         // change the checks to trigger on touching the waypoint objects
-        if (Vector2.Distance(transform.position, waypoints[nextWaypoint].position) < 1) {
+        if (Vector2.Distance(transform.position, _waypoints[nextWaypoint].position) < 1) {
             // if the employee should do something after reaching each waypoint, do that here
-            if (waypoints.Length - 1 > nextWaypoint) {
+            if (_waypoints.Length - 1 > nextWaypoint) {
                 nextWaypoint++;
-            } else if (looping) {
+            } else if (_looping) {
                 nextWaypoint = 0;
             }
         }
@@ -43,7 +43,7 @@ public class EmployeeController : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") {
             // deals damage to the player equal to the serialized damage int
-            Debug.Log(damage + " damage dealt to player");
+            Debug.Log(_damage + " damage dealt to player");
             // applies other effects to the player if relevant (e.g. knockback)
         }
     }
